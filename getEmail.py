6f1a7d72bc80e96bb3a3df628145f7c57ac19a72
Mathis <mathis.bourrat@etu.umontpellier.fr>
2020-11-17 17:18:14 +0100

    get email from email box

---
+++ getEmail.py
@@ -0,0 +1,100 @@
+import imaplib
+import base64
+import os
+import email
+import getpass
+from email.header import decode_header
+import webbrowser
+import csv 
+
+output = open('mail.csv', 'w', newline='', encoding='utf-8')
+csvwriter = csv.writer(output)
+col_names = ['expediteur', 'destinataire', 'date et heure', 'sujet', 'contenu']
+
+# https://www.thepythoncode.com/article/reading-emails-in-python
+email_user = input('Email: ')
+# email_user = "bourratmathis@gmail.com"
+email_pass = getpass.getpass('password: ')
+
+mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+N = 10
+mail.login(email_user, email_pass)
+status, messages = mail.select('INBOX')
+
+
+messages = int(messages[0])
+
+
+
+for i in range(messages, messages-N, -1):
+    mail_line = []
+    mail_line.append(email_user)
+    # fetch the email message by ID
+    res, msg = mail.fetch(str(i), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            # parse a bytes email into a message object
+            msg = email.message_from_bytes(response[1])
+            # decode the email subject
+            subject = decode_header(msg["Subject"])[0][0]
+            date = decode_header(msg["Date"])[0][0]
+            if isinstance(subject, bytes):
+                # if it's a bytes, decode to str
+                subject = subject.decode()
+            # decode email sender
+            From, encoding = decode_header(msg.get("From"))[0]
+            if isinstance(From, bytes):
+                From = From.decode(encoding)
+            mail_line.append(date)
+            mail_line.append(subject)
+            mail_line.append(From)
+            # if the email message is multipart
+            if msg.is_multipart():
+                # iterate over email parts
+                for part in msg.walk():
+                    # extract content type of email
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get("Content-Disposition"))
+                    try:
+                        # get the email body
+                        body = part.get_payload(decode=True).decode()
+                    except:
+                        pass
+                    if content_type == "text/plain" and "attachment" not in content_disposition:
+                        # print text/plain emails and skip attachments
+                        mail_line.append(body)
+                    # elif "attachment" in content_disposition:
+                    #     # download attachment
+                    #     filename = part.get_filename()
+                    #     if filename:
+                    #         if not os.path.isdir(subject):
+                    #             # make a folder for this email (named after the subject)
+                    #             os.mkdir(subject)
+                    #         filepath = os.path.join(subject, filename)
+                    #         # download attachment and save it
+                    #         open(filepath, "wb").write(part.get_payload(decode=True))
+            else:
+                # extract content type of email
+                content_type = msg.get_content_type()
+                # get the email body
+                body = msg.get_payload(decode=True).decode()
+                if content_type == "text/plain":
+                    # print only text email parts
+                    mail_line.append(body)
+            # if content_type == "text/html":
+            #     # if it's HTML, create a new HTML file and open it in browser
+            #     if not os.path.isdir(subject):
+            #         # make a folder for this email (named after the subject)
+            #         os.mkdir(subject)
+            #     filename = f"{subject[:50]}.html"
+            #     filepath = os.path.join(subject, filename)
+            #     # write the file
+            #     open(filepath, "w").write(body)
+            #     # open in the default browser
+            #     webbrowser.open(filepath)
+            # print("="*100)
+# close the connection and logout
+    csvwriter.writerow(mail_line)
+output.close()
+mail.close()
+mail.logout()
